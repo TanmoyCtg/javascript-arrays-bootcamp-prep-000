6d3b771764e53c38988b3949d0bb89da905bd5be
iftekhar joy <iftekharjoy1@gmail.com>
2018-01-14 13:39:49 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
+function addElementToBeginningOfArray(array1,element1) {
+  array1 = new Array();
+  array1.unshift(element1);
+  return array1;
+}
+function destructivelyAddElementToBeginningOfArray(array2,element2) {
+
+}
