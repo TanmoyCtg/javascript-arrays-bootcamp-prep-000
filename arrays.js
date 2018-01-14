c6dc287ea6fc8c1cbe7bd4997b433e850208d285
iftekhar joy <iftekharjoy1@gmail.com>
2018-01-14 15:41:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
-function addElementToBeginningOfArray(array1,element1) {
-  array1.unshift(element1);
-  return array1;
+function addElementToBeginningOfArray(array,element) {
+  array.unshift(element);
+  return array;
 }
 function destructivelyAddElementToBeginningOfArray(array2,element2) {
   array2.push(element2);
@@ -12,8 +12,8 @@ function addElementToBeginningOfArray() {
 
 }
 
-function destructivelyAddElementToBeginningOfArray() {
-
+function destructivelyAddElementToBeginningOfArray(array) {
+  
 }
 function removeElementFromBeginningOfArray(array) {
   var a = array.slice(-2)
@@ -22,6 +22,10 @@ function removeElementFromBeginningOfArray(array) {
 function removeElementFromEndOfArray(array) {
   var a = array.slice(0,array.length -1);
   return a;
+}
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array;
 }
 function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop();
